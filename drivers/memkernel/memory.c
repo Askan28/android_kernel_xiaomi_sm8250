61e4592172f6e49d30412a16ce75628d9b914896
Askan28 <125455151+Askan28@users.noreply.github.com>
2026-04-25 21:20:24 +0700

    Update buffer pointer increment in memory.c
    Refactor buffer pointer increment for clarity.

--- drivers/memkernel/memory.c
+++ drivers/memkernel/memory.c
@@ -180,7 +180,8 @@ ssize_t readwrite_process_memory(
 
 		count += max_chunk;
 		size -= max_chunk;
-		((char*)buffer) += max_chunk;
+		//((char*)buffer) += max_chunk;
+		buffer = (char*)buffer + max_chunk;
 		addr += max_chunk;
 	}
 	MM_READ_UNLOCK(mm);
